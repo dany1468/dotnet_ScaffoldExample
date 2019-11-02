3107258f9ca5482efa52c02e13ee4d47077782ea
dany1468 <dany1468@gmail.com>
2019-11-03 08:57:13 +0900

    Add Movie model and Context

--- Example.Models/Class1.cs
+++ Example.Models/Class1.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Example.Models
 {
-    public class Class1
+    public class MovieContext : DbContext
     {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
     }
 }
